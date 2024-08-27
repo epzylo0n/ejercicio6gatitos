e5c33c8210e845db3f5a6e0bba9efc2391d5a8fc
Diego Herrero <diego.alberto.herrero@gmail.com>
2024-08-27 16:24:01 -0300

    Resolucion ejercicio a

--- js/app.js
+++ js/app.js
@@ -18,32 +18,3 @@ for (var i = 1; i <= cantidadDeGatos; i++) {
     
     console.log("Gato #" + i + ": " + emoji);
 }
-
-// script-b.js
-
-// Punto b
-console.log("Ejercicio b):");
-
-var cantidadDeGatos = 5;
-var cantidadDePasos = 3;
-
-
-for (var i = 1; i <= cantidadDeGatos; i++) {
-    var pasos = "🐾".repeat(cantidadDePasos); 
-    console.log("Gato #" + i + ":   🐈" + pasos);
-}
-
-// script_c.js
-
-// Punto c
-console.log("Ejercicio c):");
-
-var cantidadDeGatos = 10;
-var cantidadDePasos = 4;
-
-for (var i = 1; i <= cantidadDeGatos; i++){
-    var gato = (i % 2 == 1) ? "🐈" : "🐈⬛";
-    var pasos = "🐾".repeat(cantidadDePasos);
-
-    console.log("Gato #" + i + ": " + gato + "" + pasos);
-}
